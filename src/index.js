98a050f5a043812696a6d1f92641caa2c552c853
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-12 09:07:59 -0400

    Use inline styles to style our HTML elements A brief lesson on how to use inline styles in JSX. Our instinct is to write it like HTML because that's what it looks like, but it's not. Therefore, we write inline styles like a JavaScript object, as seen in example #2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//Inline styles, example #1
+// ReactDOM.render(<h1 style={{color: "red"}}>Hello World!</h1>, document.getElementById("root"));
+
+//Inline styles, example #2 - directly injecting as a JS object
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px"
+};
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
+
+//Note that while what we're writing looks like HTML, it's not, so we can't write like HTML
+//We're writing JavaScript, so we must use JS conventions like camel case, JS objects, etc.
